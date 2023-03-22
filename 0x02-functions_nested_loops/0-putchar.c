c702a7901fda2d432d6282cc18348ae68f655a7b
Doctorstrange <doctorstrange247@gmail.com>
2023-03-21 17:34:51 -0700

    Write a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - print all number 0-9
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+char spell[8] = "_putchar";
+for (i = 0; i < 8; i++)
+{
+putchar(spell[i]);
+}
+putchar('\n');
+return (0);
+}
