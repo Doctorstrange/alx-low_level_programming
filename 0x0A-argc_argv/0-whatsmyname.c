6a4f470b8f90617b02028dfd7e1e796717556172
Doctorstrange <doctorstrange247@gmail.com>
2023-04-09 11:58:49 -0700

    Write a program that prints its name, followed by a new line.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,10 @@
  * main - the main function
  * Return: always zero
  */
-
-
 #include <stdio.h>
 
-int main() {
-   printf("%s\n", __FILE__);
-   return 0;
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
 }
