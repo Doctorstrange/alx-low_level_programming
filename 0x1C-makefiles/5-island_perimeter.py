59edf2c839d79319dd54b25b9ba85580c8530325
Doctorstrange <Doctorstrange>
2023-10-25 21:51:26 -0500

    unction def island_perimeter(grid): that returns the perimeter of the island described in grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,25 +1,38 @@
 #!/usr/bin/python3
-"""function def island_perimeter(grid): that returns the
-perimeter of the island described in grid"""
-
+"""Defines an island perimeter measuring function."""
 
 def island_perimeter(grid):
-    """function def island_perimeter(grid): that returns the
-    perimeter of the island described in grid
-    Returns: perimeter of island
-    """
+    length = len(grid[0])
+    breath = len(grid)
+    perimeter = 0 
 
-    breath = len(grid[0])
-    length = len(grid)
-    col = 0
-    size = 0
-
-    for y in range(length):
-        for x in range(breath):
+    for y in range(breath):
+        for x in range(length):
             if grid[y][x] == 1:
-                size += 1
-                if (x > 0 and grid[y][x - 1] == 1):
-                    col += 1
-                if (y > 0 and grid[y - 1][x] == 1):
-                    col += 1
-    return size * 4 - col * 2
+                perimeter += 4
+                if y == 0:
+                    if grid[y][x - 1] == 1:
+                        perimeter -= 1
+                    if grid[y][x + 1] == 1:
+                        perimeter -= 1
+                if y == len(grid):
+                    if grid[y][x - 1] == 1:
+                        perimeter -= 1
+                    if grid[y][x + 1] == 1:
+                        perimeter -= 1
+                if y > 0 and x > 0 and y != len(grid):
+                    if grid[y][x - 1] == 1:
+                        perimeter -= 1
+                    if grid[y][x + 1] == 1:
+                        perimeter -= 1
+                    if grid[y - 1][x] == 1:
+                        perimeter -= 1
+                    if grid[y + 1][x] == 1:
+                        perimeter -= 1
+                if y > 0 and y != len(grid) and x == 0:
+                    if grid[y][x + 1] == 1:
+                        perimeter -= 1
+                if y > 0 and y != len(grid) and x == len(grid[0]):
+                    if grid[y][x + 1] == 1:
+                        perimeter -= 1
+    return perimeter
