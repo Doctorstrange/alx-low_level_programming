1da2652d93f7103400f1c4ff7dc3ec762c123e3b
Doctorstrange <Doctorstrange>
2023-10-25 21:44:16 -0500

    unction def island_perimeter(grid): that returns the perimeter of the island described in grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""function def island_perimeter(grid): that returns the
+perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    length = len(grid[0])
+    breath = len(grid)
+    perimeter = 0
+
+    for y in range(breath):
+        for x in range(length):
+            if grid[y][x] == 1:
+                perimeter += 4
+                if y == 0:
+                    if grid[y][x - 1] == 1:
+                        perimeter -= 1
+                    if grid[y][x + 1] == 1:
+                        perimeter -= 1
+                if y == len(grid):
+                    if grid[y][x - 1] == 1:
+                        perimeter -= 1
+                    if grid[y][x + 1] == 1:
+                        perimeter -= 1
+                if y > 0 and x > 0 and y != len(grid):
+                    if grid[y][x - 1] == 1:
+                        perimeter -= 1
+                    if grid[y][x + 1] == 1:
+                        perimeter -= 1
+                    if grid[y - 1][x] == 1:
+                        perimeter -= 1
+                    if grid[y + 1][x] == 1:
+                        perimeter -= 1
+                if y > 0 and y != len(grid) and x == 0:
+                    if grid[y][x + 1] == 1:
+                        perimeter -= 1
+                if y > 0 and y != len(grid) and x == len(grid[0]):
+                    if grid[y][x + 1] == 1:
+                        perimeter -= 1
+    return perimeter
